d19fb7d87788710bfa866304cebee41c8811d712
HAE2NI <haein990222@naver.com>
2023-03-30 17:43:50 +0900

    💄 onBoarding ui

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { View, Text } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
 import ChooseJob from "./src/screens/OnBoarding/ChooseJob";
 import styled from "styled-components/native";
+import ChooseMoney from "./src/screens/OnBoarding/ChooseMoney";
 
 export default function App() {
-  return <ChooseJob />;
+  return <ChooseMoney />;
 }
 
 const Hello = styled.View`
