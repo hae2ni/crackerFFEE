e2f241cd2c41bb0f21cb002e07fc54754488155d
HAE2NI <hae0_02ni@ewhain.net>
2023-04-26 23:15:01 +0900

    🎨 navigation stack 추가

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { View, Text } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
+import JobNickname from "./src/screens/OnBoarding/JobNickname";
 import ChooseJob from "./src/screens/OnBoarding/ChooseJob";
 import styled from "styled-components/native";
 import ChooseMoney from "./src/screens/OnBoarding/ChooseMoney";
+import ChooseTime from "./src/screens/OnBoarding/ChooseTime";
+import { GlobalStyle } from "./src/styles/Global";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
-  return <ChooseMoney />;
+  return (
+    <NavigationContainer>
+      <GlobalStyle />
+      <Stack.Navigator initialRouteName="onBoarding">
+        <Stack.Screen name="JobNickname" component={JobNickname} />
+        <Stack.Screen name="ChooseJob" component={ChooseJob} />
+        <Stack.Screen name="ChooseTime" component={ChooseTime} />
+        <Stack.Screen name="ChooseMoney" component={ChooseMoney} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const Hello = styled.View`
-  background-color: red;
-`;
